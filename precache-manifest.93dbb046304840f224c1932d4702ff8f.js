f875d3d88aec2bd891992203f790b3f2090eea61
Greg <gmerrill03@gmail.com>
2019-10-07 13:31:58 -0700

    Updates

---
+++ precache-manifest.93dbb046304840f224c1932d4702ff8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016da964cd2f8f25631c8d43d716754e",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "570521640b676b7c2cd6",
+    "url": "/project-4-assessment/static/css/main.94f03965.chunk.css"
+  },
+  {
+    "revision": "adcc6c9126018b755d0a",
+    "url": "/project-4-assessment/static/js/2.25804772.chunk.js"
+  },
+  {
+    "revision": "570521640b676b7c2cd6",
+    "url": "/project-4-assessment/static/js/main.e4cc07e8.chunk.js"
+  },
+  {
+    "revision": "75de8ec33a8a78802050",
+    "url": "/project-4-assessment/static/js/runtime-main.71652b97.js"
+  }
+]);
